a16a69219fcd1c686b68cf9603b8d13c44ce7764
RWEMAREMY <rwemaremy21@gmail.com>
2024-12-21 00:04:43 +0200

    new title

--- src/components/About/videosection.tsx
+++ src/components/About/videosection.tsx
@@ -5,7 +5,7 @@ const VideoSection: React.FC = () => {
  return (
         <div className="video-section flex flex-col items-center">
             {/* Video Display */}
-            <h2 className="text-3xl font-bold mb-4 underline">THE STORY WE HAVE GOT</h2>
+            <h2 className="text-3xl font-bold mb-4 underline">THE STORY OF US</h2>
             <div className="video-container pl-[10px] mb-4">
                 <video className="w-full max-w-[600px]" controls>
                     <source src={alinevid} type="video/mp4" />
